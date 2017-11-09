16ee8adc84cfd3c99c4b1c98bbd025622cb49d1a
Eric Butler <ericbutler555@gmail.com>
2017-11-09 00:19:53 -0500

    First commit

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// When the button is clicked, pop-up an alert with a supportive message:
+document.querySelector('.cta').addEventListener('click', function() {
+  alert("Thanks for clicking - Let's do it!");
+}, false);
